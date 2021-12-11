0923ca2abc34413be60ec34b5de948435b80a05d
LEE <sunkind0709@gmail.com>
2021-12-11 11:30:46 +0900

    updata_001

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+    //일단 이름을 정한다 복수니까 무비스.
+    //{}안에는 함수, 상태값들이 들어간다.
+    const [movies, setMovies] = useState([])
+
     return (
         <div className={"App"}>
-          <h1>
-            123456
-          </h1>
+            <h1>
+                {movies.length}
+            </h1>
+            {movies.map(movie => (
+                <h1>
+                    {movie.title}
+                </h1>
+            ))}
         </div>
     );
 };
